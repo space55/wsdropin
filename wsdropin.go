c294e1b4716d25955cacd0b52892a20e9a396fe0
Eamonn Nugent <eamonn@enugent.consulting>
2020-10-13 17:28:45 -0400

    Make the struct extend net.Listener

--- wsdropin.go
+++ wsdropin.go
@@ -14,8 +14,8 @@ func Dial(addr string) (net.Conn, error) {
 }
 
 type WSServer struct {
-	listener net.Listener
-	conns    chan net.Conn
+	net.Listener
+	conns chan net.Conn
 }
 
 func Listen(laddr string) (net.Listener, error) {
@@ -24,8 +24,8 @@ func Listen(laddr string) (net.Listener, error) {
 		return nil, err
 	}
 	wss := &WSServer{
-		listener: listener,
-		conns:    make(chan net.Conn),
+		listener,
+		make(chan net.Conn),
 	}
 	http.Handle("/", websocket.Handler(wss.wsHandler))
 	err = http.Serve(listener, nil)
@@ -41,12 +41,12 @@ func (w *WSServer) Accept() (net.Conn, error) {
 }
 
 func (w *WSServer) Close() error {
-	return w.listener.Close()
+	return w.Close()
 }
 
 func (w *WSServer) Addr() net.Addr {
 	// This is still legit enough? Maybe?
-	return w.listener.Addr()
+	return w.Addr()
 }
 
 func (w *WSServer) wsHandler(ws *websocket.Conn) {
